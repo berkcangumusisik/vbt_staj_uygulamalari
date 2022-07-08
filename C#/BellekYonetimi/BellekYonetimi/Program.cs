be1adbc2f6b599c3d6ddbf6ab9132dc717f343ab
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2022-07-08 08:38:54 +0300

    memory managment 1 uptaded.

--- C#/BellekYonetimi/BellekYonetimi/Program.cs
+++ C#/BellekYonetimi/BellekYonetimi/Program.cs
@@ -1,11 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+byte x = 20;
+int y;
+y = x;
+
+float f = 20;
+double d;
+d = f;
+
+int t = 5;
+byte m = 3;
+short c;
+
+int k = 5;
+byte l = 3;
+short r = 30;
+double g = k + l + r;
+
+Char c2 = 'a';
+decimal u;
+u = c2;
+Console.WriteLine(u);
 
 byte a = 5;
 byte b = 10;
-byte c = (byte)(a + b);
-Console.WriteLine(c);
+byte ı = (byte)(a + b);
+Console.WriteLine(ı);
 
 int i = 256;
 byte by;
@@ -15,8 +36,8 @@ Console.WriteLine(by);
 Object o = 5;
 int j = (int)o;
 
-int k = 50;
-Object o2 = (Object)k;
+int p = 50;
+Object o2 = (Object)p;
 
 
 Console.ReadLine();
