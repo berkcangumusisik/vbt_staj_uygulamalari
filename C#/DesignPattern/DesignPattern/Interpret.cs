36f658ebfb9b1c88f9806701ab080f116a528f90
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2022-07-08 13:38:08 +0300

    Design Pattern Project Added.

---
+++ C#/DesignPattern/DesignPattern/Interpret.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    public class Interpret
+    {
+        public interface Expression
+        {
+            bool Interpret(string content);
+        }
+
+        public class CheckExpression : Expression
+        {
+            private string _word;
+            public CheckExpression(string word)
+            {
+                _word = word;
+            }
+
+            public bool Interpret(string content)
+            {
+                return content.ToLower().Contains(_word.ToLower());
+            }
+        }
+
+        public class OrExpression : Expression
+        {
+            private Expression _exp1;
+            private Expression _exp2;
+            public OrExpression(Expression exp1, Expression exp2)
+            {
+                _exp1 = exp1;
+                _exp2 = exp2;
+            }
+
+
+            public bool Interpret(string content)
+            {
+                return (_exp1.Interpret(content) || _exp2.Interpret(content));
+            }
+        }
+
+        public class AndExpression : Expression
+        {
+            private Expression _exp1;
+            private Expression _exp2;
+            public AndExpression(Expression exp1, Expression exp2)
+            {
+                _exp1 = exp1;
+                _exp2 = exp2;
+            }
+
+            public bool Interpret(string content)
+            {
+                return (_exp1.Interpret(content) && _exp2.Interpret(content));
+            }
+        }
+
+        public class And3Expression : Expression
+        {
+            private Expression _exp1;
+            private Expression _exp2;
+            private Expression _exp3;
+            public And3Expression(Expression exp1, Expression exp2, Expression exp3)
+            {
+                _exp1 = exp1;
+                _exp2 = exp2;
+                _exp3 = exp3;
+            }
+
+            public bool Interpret(string content)
+            {
+                return (_exp1.Interpret(content) && _exp2.Interpret(content) && _exp3.Interpret(content));
+            }
+        }
+
+        public class InterpretPattern
+        {
+            public static Expression getMaleExpression()
+            {
+                Expression futbol = new CheckExpression("futbol");
+                Expression araba = new CheckExpression("araba");
+                return new OrExpression(futbol, araba);
+
+            }
+
+            public static List<Expression> getFemaleExpression()
+            {
+                Expression anne = new CheckExpression("anne");
+                Expression bebe = new CheckExpression("bebe");
+                Expression sürt = new CheckExpression("surt");
+                Expression araba = new CheckExpression("araba");
+                Expression çiz = new CheckExpression("çiz");
+                Expression güzellik = new CheckExpression("güzellik");
+                Expression makyaj = new CheckExpression("makyaj");
+                List<Expression> list = new();
+                list.Add(new OrExpression(anne,bebe));
+                list.Add(new AndExpression(sürt,araba));
+                list.Add(new AndExpression(çiz,araba));
+                list.Add(new OrExpression(güzellik,makyaj));
+                return list;
+                list.Add(new And3Expression(güzellik, makyaj, çiz));
+
+
+            }
+        }
+    }
+}
---
+++ C#/DesignPattern/DesignPattern/Program.cs
@@ -0,0 +1,34 @@
+﻿using static DesignPattern.Interpret;
+
+string Description1 = "Futbol maçlarını çok sevsem de genelde izlemiyorum";
+string Description2 = "Bebeğimin sağlığı için anne sütüne çok önem veriyorum.";
+string Description3 = "Spor'a giderken arabayı güzellikle vurdum";
+
+Console.WriteLine($"{Description1} (Erkek):");
+Console.WriteLine(InterpretPattern.getMaleExpression().Interpret(Description1));
+bool expResult = false;
+Console.WriteLine($"{Description2} (Kadın):");
+foreach (Expression exp in InterpretPattern.getFemaleExpression())
+{
+    if (exp.Interpret(Description2))
+    {
+        expResult = true;
+        break;
+    }
+    
+}
+
+Console.WriteLine(expResult);
+
+bool expResult2 = false;
+Console.WriteLine($"{Description3} (Kadın):");
+foreach (Expression exp in InterpretPattern.getFemaleExpression())
+{
+    if (exp.Interpret(Description3))
+    {
+        expResult2 = true;
+        break;
+    }
+    
+}
+Console.WriteLine(expResult2);
